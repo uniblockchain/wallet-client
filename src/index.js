6cc9892ee64a0d966e2b5c83c0aef04897db71ec
Erko Risthein <erko@risthein.ee>
2017-10-10 18:02:59 +0300

    Added redux store, redux devtools, configured react router and airbnb eslint settings

--- src/index.js
+++ src/index.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import {
+  ConnectedRouter,
+  routerReducer,
+  routerMiddleware
+} from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router';
+
+// import { userReducer } from './user';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import tracker from './tracker';
+import './index.css';
+
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const history = createHistory();
+
+const rootReducer = combineReducers({
+  router: routerReducer
+  // ...otherReducers,
+});
+
+const middleware = [
+  routerMiddleware(history)
+  // ...otherMiddleware,
+];
+
+const storeEnhancer = composeEnhancers(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, storeEnhancer);
 
 tracker.initialize();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <ReduxProvider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <Route exact path="/" component={App} />
+        {/* <Route path="/about" component={About}/> */}
+      </div>
+    </ConnectedRouter>
+  </ReduxProvider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
