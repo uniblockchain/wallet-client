0da8b5ac654e67801fa6c4adf406ada1738b647d
Erko Risthein <erko@risthein.ee>
2017-10-12 18:24:16 +0300

    Fixed test warnings

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,7 @@
 // @flow
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-extraneous-dependencies,import/first */
 
+import './shim'; // TODO: remove after "react-scripts": "1.0.14",
 import 'jest-enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
