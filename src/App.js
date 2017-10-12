a6dd5fef2aa5a57e1e1ab31fd67abc27d831056c
Erko Risthein <erko@risthein.ee>
2017-10-12 18:57:01 +0300

    Added Row & Col

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 // @flow
 
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Button, Col, Row } from 'reactstrap';
 import './App.css';
 
 const App = () => (
-  <div className="app">
-    <h1 className="mb-3">Welcome to Change.</h1>
-    <h1>Your digital wallet.</h1>
-    <div className="bottom">
-      <Button color="primary" size="lg" block>
-        Sign up
-      </Button>
-      <Button color="primary" size="lg" block>
-        Log in
-      </Button>
-    </div>
+  <div>
+    <Row className="justify-content-md-center">
+      <Col className="app col-lg-6">
+        <h1 className="mb-3">Welcome to Change.</h1>
+        <h1>Your digital wallet.</h1>
+        <div className="bottom">
+          <Button color="primary" size="lg" block>
+            Sign up
+          </Button>
+          <Button color="primary" size="lg" block>
+            Log in
+          </Button>
+        </div>
+      </Col>
+    </Row>
   </div>
 );
 
