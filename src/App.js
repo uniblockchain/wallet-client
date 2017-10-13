c525a72a87f14b2c9aa57a65fb1b81e1bbbf472e
Erko Risthein <erko@risthein.ee>
2017-10-13 13:24:23 +0300

    Added Row & Col

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@
 
 import React from 'react';
 import { Button, Col, Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 const App = () => (
   <div>
     <Row className="justify-content-md-center">
-      <Col className="app col-lg-6">
+      <Col className="app col-lg-4">
         <h1 className="mb-3">Welcome to Change.</h1>
         <h1>Your digital wallet.</h1>
         <div className="bottom">
-          <Button color="primary" size="lg" block>
-            Sign up
-          </Button>
-          <Button color="primary" size="lg" block>
-            Log in
-          </Button>
+          <Link to="/wallet">
+            <Button color="primary" size="lg" block>
+              Sign up
+            </Button>
+          </Link>
+          <Link to="/wallet">
+            <Button color="primary" size="lg" block btn-raised>
+              Log in
+            </Button>
+          </Link>
         </div>
       </Col>
     </Row>
--- src/wallet/Wallet.js
+++ src/wallet/Wallet.js
@@ -2,8 +2,11 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Col, Row } from 'reactstrap';
+import { Doughnut } from 'react-chartjs-2';
 import walletActions from './walletActions';
 // import type { WalletFetchRequest } from './walletActionTypes';
+import './Wallet.css';
 
 type Props = {
   wallets: Array<{}>,
@@ -15,8 +18,50 @@ export class Wallet extends Component<Props> {
     this.props.fetchWallet();
   }
 
+  data = {
+    datasets: [
+      {
+        data: [10, 20, 30],
+        backgroundColor: ['#19c3ed', '#47d6e2', '#62dfd9'],
+        borderWidth: [0, 0, 0],
+      },
+    ],
+    labels: ['BTC', 'ETH', 'CAG'],
+  };
+
+  options = {
+    circumference: 10 / 6 * Math.PI,
+    rotation: 1 / 6 * Math.PI,
+    legend: {
+      display: false,
+    },
+  };
+
   render() {
-    return <div>yoyoy</div>;
+    return (
+      <div>
+        <Row className="justify-content-md-center">
+          <Col className="wallet col-lg-4">
+            <div className="top">
+              <div className="text-right">
+                <button type="button" className="btn btn-primary bmd-btn-icon">
+                  <i className="material-icons">more_horiz</i>
+                </button>
+              </div>
+              <Doughnut data={this.data} options={this.options} />
+            </div>
+            <div className="bottom">
+              <nav className="navbar navbar-light bg-light">
+                <i className="material-icons md-dark md-48">home</i>
+                <i className="material-icons md-dark md-48">account_balance</i>
+                <i className="material-icons md-dark md-48">credit_card</i>
+                <i className="material-icons md-dark md-48">apps</i>
+              </nav>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    );
   }
 }
 
