67d25dc02e6138428d9593557d1de3b88057458b
Maido Käära <maidokaara@gmail.com>
2017-10-27 15:26:09 +0300

    Fixed header colors and sizes

--- src/ui/headers.js
+++ src/ui/headers.js
@@ -3,10 +3,14 @@ import styled from 'styled-components';
 
 export const Header = styled.h1`
   font-family: 'CircularStdBold', Fallback, sans-serif;
-  font-size: 30px;
-  color: #083b70;
+  font-size: 36px;
+  color: ${props => props.theme.altText};
 `;
 
-export const SubHeader = styled.h2``;
+export const SubHeader = styled.h2`
+  font-family: 'CircularStdBold', Fallback, sans-serif;
+  font-size: 30px;
+  color: ${props => props.theme.altText};
+`;
 
 export default { Header, SubHeader };
--- src/ui/theme.js
+++ src/ui/theme.js
@@ -2,16 +2,19 @@
 export type Theme = {
   background: string,
   text: string,
+  altText: string,
 };
 
 export const BlueTheme: Theme = {
   background: '#c6f3ff',
   text: '#083b70',
+  altText: '#02bda5',
 };
 
 export const GreenTheme: Theme = {
   background: '#e5f9f3',
   text: '#02bda5',
+  altText: '#083b70',
 };
 
 export default { GreenTheme, BlueTheme };
