758d9665b49615a84eef412f6a2d8f766922a6ae
Maido Käära <maidokaara@gmail.com>
2017-10-31 14:49:46 +0200

    Added all fonts

--- src/ui/text.js
+++ src/ui/text.js
@@ -2,13 +2,11 @@
 import styled from 'styled-components';
 
 export const Header = styled.h1`
-  font-family: 'CircularStdBold', Fallback, sans-serif;
   font-size: 36px;
   color: ${props => props.theme.altText};
 `;
 
 export const SubHeader = styled.h2`
-  font-family: 'CircularStdBold', Fallback, sans-serif;
   font-size: 30px;
   color: ${props => props.theme.altText};
 `;
