25f16808c7a6302f7a00f49a818e31ef78c121dd
Maido Käära <maidokaara@gmail.com>
2017-11-02 16:27:28 +0200

    Staging deploy

--- src/configuration/configuration.js
+++ src/configuration/configuration.js
@@ -21,11 +21,12 @@ const DEFAULT_CONF_PRODUCTION = {
 };
 
 export default function initialize(): void {
-  if (process.env.NODE_ENV === 'development') {
+  const env = process.env.REACT_APP_ENV || process.env.NODE_ENV;
+  if (env === 'development') {
     config.set(DEFAULT_CONF_DEV, { freeze: false });
-  } else if (process.env.NODE_ENV === 'staging') {
+  } else if (env === 'staging') {
     config.set(DEFAULT_CONF_STAGING, { freeze: false });
-  } else if (process.env.NODE_ENV === 'production') {
+  } else if (env === 'production') {
     config.set(DEFAULT_CONF_PRODUCTION, { freeze: false });
   }
 }
