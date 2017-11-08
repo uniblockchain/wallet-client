2cc481211242061ef9dd4c83a355cfefe51ad78a
Maido Käära <maidokaara@gmail.com>
2017-11-08 17:15:57 +0200

    Test fix

--- src/wallet/Wallet.spec.js
+++ src/wallet/Wallet.spec.js
@@ -2,11 +2,33 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Wallet } from './Wallet';
+import type { Wallet as WalletType } from './walletState';
 
 describe('Wallet component', () => {
   let component;
 
-  const props = {};
+  const wallet: WalletType = {
+    id: 1,
+    currency: 'BTC',
+    address: '',
+    transactions: [],
+    receiveAddress: '2MvpyDrvrV3PNRTD8cBX9Hy97s7NtBSGfEN',
+    balance: [
+      {
+        value: 0.19890018,
+        currency: 'BTC',
+      },
+      {
+        value: 1257.71,
+        currency: 'EUR',
+      },
+    ],
+  };
+
+  const props = {
+    wallet,
+    representationalCurrency: 'EUR',
+  };
 
   beforeEach(() => {
     component = shallow(<Wallet {...props} />);
