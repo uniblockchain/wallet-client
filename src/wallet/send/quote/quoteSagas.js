3aaf1a4dc4959df9f955eb8e5068d75ccea9aff6
Erko Risthein <erko@risthein.ee>
2017-11-08 15:02:26 +0200

    Fix decimal place error in the Send form

--- src/wallet/send/quote/quoteSagas.js
+++ src/wallet/send/quote/quoteSagas.js
@@ -12,8 +12,12 @@ export function* getQuote(
   try {
     const quote = yield call(quoteApi.getQuote, action.quote);
     yield put(quoteActions.getQuoteSucceeded(quote));
-    yield put(change('send', 'amountInFiat', quote.toValue, true, false));
-    yield put(change('send', 'amountInCrypto', quote.fromValue, true, false));
+    if (action.quote.fromValue) {
+      yield put(change('send', 'amountInFiat', quote.toValue, true, false));
+    }
+    if (action.quote.toValue) {
+      yield put(change('send', 'amountInCrypto', quote.fromValue, true, false));
+    }
   } catch (error) {
     yield put(quoteActions.getQuoteFailed(error.message));
     console.error(error);
