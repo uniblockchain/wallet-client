fbf600473032d938c791460d72a6a8477fd02598
Maido Käära <maidokaara@gmail.com>
2017-11-14 14:50:04 +0200

    Button specs

--- src/ui/buttons.story.spec.js
+++ src/ui/buttons.story.spec.js
@@ -1,19 +1,47 @@
 // @flow
 import React from 'react';
-import { storiesOf, action, boolean } from '../../.storybook/facade';
+import { shallow } from 'enzyme';
+import {
+  storiesOf,
+  action,
+  boolean,
+  specs,
+  describe,
+  beforeEach,
+  it,
+} from '../../.storybook/facade';
 import { Button, PrimaryButton } from './buttons';
 
-storiesOf('Controls', module).add('Button', () => (
-  <div>
-    <Button inline={boolean('Inline', false)} onClick={action('clicked')}>
-      Normal button
-    </Button>
-    <PrimaryButton
-      alt={boolean('Alternative', false)}
-      inline={boolean('Inline', false)}
-      onClick={action('clicked')}
-    >
-      Primary button
-    </PrimaryButton>
-  </div>
-));
+storiesOf('Controls', module).add('Button', () => {
+  specs(() =>
+    describe('Button', () => {
+      let component;
+
+      beforeEach(() => {
+        component = shallow(<Button />);
+      });
+      it('renders button component', () => {
+        expect(component);
+      });
+      it('renders primary button component', () => {
+        const primaryButton = shallow(<PrimaryButton />);
+        expect(primaryButton);
+      });
+    }),
+  );
+
+  return (
+    <div>
+      <Button inline={boolean('Inline', false)} onClick={action('clicked')}>
+        Normal button
+      </Button>
+      <PrimaryButton
+        alt={boolean('Alternative', false)}
+        inline={boolean('Inline', false)}
+        onClick={action('clicked')}
+      >
+        Primary button
+      </PrimaryButton>
+    </div>
+  );
+});
