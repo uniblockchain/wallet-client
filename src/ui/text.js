84d42ecea29bc940e4def72422ae2248986a5bee
Maido Käära <maidokaara@gmail.com>
2017-11-14 14:16:02 +0200

    Alt property support

--- src/ui/text.js
+++ src/ui/text.js
@@ -1,17 +1,29 @@
 // @flow
+import React from 'react';
 import styled from 'styled-components';
 
-export const Header = styled.h1`
+type Props = {
+  alt: boolean,
+  children: any,
+};
+
+const H1 = ({ alt, ...props }: Props) => <h1 {...props}>{props.children}</h1>;
+
+export const Header = styled(H1)`
   font-size: 30px;
   color: ${props => (props.alt ? props.theme.alt : props.theme.main)};
 `;
 
-export const SubHeader = styled.h2`
+const H2 = ({ alt, ...props }: Props) => <h2 {...props}>{props.children}</h2>;
+
+export const SubHeader = styled(H2)`
   font-size: 30px;
   color: ${props => (props.alt ? props.theme.alt : props.theme.main)};
 `;
 
-export const Paragraph = styled.p`
+const P = ({ alt, ...props }: Props) => <p {...props}>{props.children}</p>;
+
+export const Paragraph = styled(P)`
   font-size: 16px;
   color: ${props => (props.alt ? props.theme.altText : props.theme.text)};
 `;
