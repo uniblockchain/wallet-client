02bc307c443fb4b857dc31d4a3d6bd65a89d4cb2
Jordan Valdma <jordan.valdma@gmail.com>
2017-11-17 12:49:50 +0200

    Tracks mixpanel events only in production

--- src/tracker/Tracker.js
+++ src/tracker/Tracker.js
@@ -4,13 +4,20 @@ import config from 'react-global-configuration';
 import mixpanel from 'mixpanel-browser';
 import { initialize as initializeErrorTracking } from './ErrorTracker';
 
+const isProduction = (): boolean =>
+  (process.env.REACT_APP_ENV || process.env.NODE_ENV) === 'production';
+
 export function initialize(): void {
   initializeErrorTracking();
-  mixpanel.init(config.get('mixpanelToken'));
+  if (isProduction()) {
+    mixpanel.init(config.get('mixpanelToken'));
+  }
 }
 
-export function track(event: string, data: Object): void {
-  mixpanel.track(event, data);
+export function track(event: string, data: ?Object = undefined): void {
+  if (isProduction()) {
+    mixpanel.track(event, data);
+  }
 }
 
 export default { initialize, track };
--- src/tracker/Tracker.spec.js
+++ src/tracker/Tracker.spec.js
@@ -1,20 +1,48 @@
 // @flow
 
+import mixpanel from 'mixpanel-browser';
 import config from 'react-global-configuration';
 
 const mockErrorTracker = jest.genMockFromModule('./ErrorTracker');
 jest.mock('./ErrorTracker', () => mockErrorTracker);
 
-const { initialize } = require('./Tracker');
+const { initialize, track } = require('./Tracker');
 
 describe('tracker', () => {
   beforeEach(() => {
-    config.set({ mixpanelToken: 'some token' });
     mockErrorTracker.initialize.mockClear();
   });
 
   it('can initialize', () => {
+    config.set({ mixpanelToken: 'some token' });
     initialize();
     expect(mockErrorTracker.initialize).toHaveBeenCalledTimes(1);
+  });
+
+  describe('tracks in production', () => {
+    beforeEach(() => {
+      process.env.REACT_APP_ENV = 'production';
+    });
+
+    afterEach(() => {
+      process.env.REACT_APP_ENV = undefined;
+    });
+
+    it('can track an event', () => {
+      const event = 'SAMPLE_EVENT';
+
+      track(event);
+      expect(mixpanel.track).toHaveBeenCalled();
+      expect(mixpanel.track).toHaveBeenCalledWith(event, undefined);
+    });
+
+    it('can track an event with data', () => {
+      const event = 'SAMPLE_EVENT';
+      const data = { sample: 'data' };
+
+      track(event, data);
+      expect(mixpanel.track).toHaveBeenCalled();
+      expect(mixpanel.track).toHaveBeenCalledWith(event, data);
+    });
   });
 });
