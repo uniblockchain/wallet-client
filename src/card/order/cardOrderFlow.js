40b8ac945d5e8b565e7f917f279cf82f4138f7e4
Maido Käära <maidokaara@gmail.com>
2017-12-13 18:01:35 +0200

    Card order flow screen height fix

--- src/card/order/cardOrderFlow.js
+++ src/card/order/cardOrderFlow.js
@@ -36,7 +36,7 @@ export const NavigationDotsContainer = styled.div`
 
 const Container = styled.div`
   background-color: white;
-  height: 100vh;
+  min-height: 100vh;
   width: 100vw;
   display: flex;
   flex-direction: column;
