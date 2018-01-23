64403d75f6dbebda70db99b5a1bf9281bf0c2181
Maido Käära <maidokaara@gmail.com>
2018-01-23 13:22:53 +0200

    Added polyfills for most common JS errors

--- src/polyfills.js
+++ src/polyfills.js
@@ -2,3 +2,6 @@
 
 import 'number-to-locale-string';
 import 'string.prototype.includes';
+import 'es6-set/implement';
+import 'jspolyfill-array.prototype.find';
+import 'jspolyfill-array.prototype.findIndex';
