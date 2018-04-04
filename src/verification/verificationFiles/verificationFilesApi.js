bcb38e3cafe760de8d4b004e8532d457626fe031
Erko Risthein <erko@risthein.ee>
2018-04-04 14:54:58 +0300

    Fix verification upload endpoint urls

--- src/verification/verificationFiles/verificationFilesApi.js
+++ src/verification/verificationFiles/verificationFilesApi.js
@@ -1,5 +1,6 @@
 // @flow
 
+import config from 'react-global-configuration';
 import { post } from '../../http';
 
 export type VerificationFile = {
@@ -12,19 +13,25 @@ export type VerificationFileBase64 = {
   +base64Data: string,
 };
 
+const apiEndpoint = '/v1/verification-files';
+const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
 export const uploadFile = (
   verificationFile: VerificationFile,
 ): Promise<VerificationFile> => {
   const formData = new FormData();
   formData.append('type', verificationFile.type);
   formData.append('file', verificationFile.file);
-  return post('/v1/verification-files', formData);
+  return post(
+    iOS ? apiEndpoint : `${config.get('apiUrl')}${apiEndpoint}`,
+    formData,
+  );
 };
 
 export const postFile = (
   verificationFile: VerificationFileBase64,
 ): Promise<VerificationFile> => {
-  return post('/v1/verification-files', verificationFile);
+  return post(`${config.get('apiUrl')}${apiEndpoint}`, verificationFile);
 };
 
 export default { uploadFile, postFile };
