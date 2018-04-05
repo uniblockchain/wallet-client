7e4c94828632cd3111547da68676313c09304205
klaidoja <kadi.laidoja@gmail.com>
2018-04-05 13:45:48 +0300

    Removed isVerified check from VerificationButton component

--- src/overview/verificationButton/VerificationButton.js
+++ src/overview/verificationButton/VerificationButton.js
@@ -1,41 +1,27 @@
 // @flow
 
 import React from 'react';
-import { connect } from 'react-redux';
 import styled from 'styled-components';
-import type { MapStateToProps } from 'react-redux';
 import { Card, GradientButton, Link } from '../../ui';
-import withUser from '../../user/withUser';
 import { VERIFICATION_INTRO_ROUTE } from '../../verification/constants';
 import svgRocket from './img/rocket.svg';
-
-export type Props = {
-  isVerified: boolean,
-};
 
 const Rocket = styled.img`
   margin: 0 0 3px 2px;
 `;
 
-export const VerificationButton = ({ isVerified }: Props) => (
+export const VerificationButton = () => (
   <div>
-    {!isVerified && (
-      <Card>
-        <div className="text-center">
-          <GradientButton inline className="mt-3">
-            <Link to={VERIFICATION_INTRO_ROUTE}>
-              Verify account{' '}
-              <Rocket height="16px" src={svgRocket} alt="Rocket" />
-            </Link>
-          </GradientButton>
-        </div>
-      </Card>
-    )}
+    <Card>
+      <div className="text-center">
+        <GradientButton inline className="mt-3">
+          <Link to={VERIFICATION_INTRO_ROUTE}>
+            Verify account <Rocket height="16px" src={svgRocket} alt="Rocket" />
+          </Link>
+        </GradientButton>
+      </div>
+    </Card>
   </div>
 );
 
-const mapStateToProps: MapStateToProps<*, Props, *> = state => ({
-  isVerified: state.user.isVerified,
-});
-
-export default withUser(connect(mapStateToProps)(VerificationButton));
+export default VerificationButton;
